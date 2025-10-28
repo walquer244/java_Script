a2a9cd022b1eba655f18291107f50cec36424911
walquer244 <walquervasconcelos@gmail.com>
2025-10-28 14:12:11 -0300

    Add files via upload

---
+++ java_Script/atividade.js
@@ -0,0 +1,10 @@
+let numero1 = 20
+let numero2 = 45
+
+if (numero1 > numero2){
+    console.log(`o ${numero1} é maior que o ${numero2} `)
+}else if (numero1 < numero2){
+    console.log(`o ${numero1} é menor que o ${numero2} `)
+}else{
+    console.log("Os dois valores são iguais")
+}
